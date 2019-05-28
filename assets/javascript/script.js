b8ef37cdc584781ea8e66cc4258466fe424a2cb6
Aaron Keisler <amkeisler@gmail.com>
2019-05-28 16:55:31 -0600

    add climbing images and tweak JS to add on win

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,40 +2,97 @@
 let wins = 0;
 let losses = 0;
 let usedGuesses = [];
-let remaining = 8;
+let remaining = 5;
 let targetWord;
 let wordArr;
 let guess;
 let spanArray = [];
 let wordHint;
+let winPicSrc;
 const wordList = [
-  { word: "anchor", hint: "better make these bombproof" },
-  { word: "arete", hint: "climbing up the corner" },
-  { word: "belayer", hint: "alex honnold doesn't always need one of these" },
-  { word: "beta", hint: "don't spray" },
-  { word: "bomber", hint: "totems" },
-  { word: "camalot", hint: "C4" },
-  { word: "chimney", hint: "first few pitches of epinephrine" },
-  { word: "chock", hint: "nuts" },
-  { word: "dihedral", hint: "triple-s" },
-  { word: "flash", hint: "first time, but not an onsight" },
-  { word: "gaston", hint: "love these moves" },
-  { word: "heelhook", hint: "always gotta use those heels" },
-  { word: "highball", hint: "sketchier than trad" },
-  { word: "leader", hint: "the sharp end" },
-  { word: "lockoff", hint: "master this and you'll be set" },
-  { word: "mantel", hint: "fitz caldwell has this down" },
-  { word: "offwidth", hint: "chicken wings and body jams" },
-  { word: "pumped", hint: "basically any climb at the RRG" },
-  { word: "quickdraw", hint: "need this for sport climbing" },
-  { word: "redpoint", hint: "gotta send" },
-  { word: "route", hint: "bouldering is stupid" },
-  { word: "runout", hint: "get that trad head" },
-  { word: "sidepull", hint: "hope it's a jug" },
-  { word: "sloper", hint: "definitely not a jug" },
-  { word: "toprope", hint: "the most badass" },
-  { word: "traverse", hint: "pitch 15, dawn wall" },
-  { word: "whipper", hint: "always take the whip" }
+  {
+    word: "anchor",
+    hint: "better make these bombproof",
+    img: "./images/anchor.jpg"
+  },
+  { word: "arete", hint: "climbing up the corner", img: "./images/arete.jpg" },
+  {
+    word: "belayer",
+    hint: "alex honnold doesn't always need one of these",
+    img: "./images/"
+  },
+  { word: "beta", hint: "don't spray", img: "./images/beta.jpg" },
+  { word: "bomber", hint: "totems", img: "./images/bomber.jpg" },
+  {
+    word: "bouldering",
+    hint: "this is dumb...compared to routes",
+    img: "./images/bouldering.jpg"
+  },
+  { word: "camalot", hint: "C4", img: "./images/camalot.jpg" },
+  {
+    word: "chimney",
+    hint: "first few pitches of epinephrine",
+    img: "./images/chimney.jpg"
+  },
+  { word: "chock", hint: "nuts", img: "./images/chock.gif" },
+  { word: "dihedral", hint: "triple-s", img: "./images/dihedral.jpg" },
+  {
+    word: "flash",
+    hint: "first time, but not an onsight",
+    img: "./images/flash.jpg"
+  },
+  { word: "gaston", hint: "love these moves", img: "./images/gaston.jpg" },
+  {
+    word: "heelhook",
+    hint: "always gotta use those heels",
+    img: "./images/heelhook.jpg"
+  },
+  {
+    word: "highball",
+    hint: "sketchier than trad",
+    img: "./images/highball.jpg"
+  },
+  { word: "leader", hint: "the sharp end", img: "./images/leader.jpg" },
+  {
+    word: "lockoff",
+    hint: "master this and you'll be set",
+    img: "./images/lockoff.jpg"
+  },
+  {
+    word: "mantel",
+    hint: "fitz caldwell has this down",
+    img: "./images/mantel.png"
+  },
+  {
+    word: "offwidth",
+    hint: "chicken wings and body jams",
+    img: "./images/offwidth.jpg"
+  },
+  {
+    word: "pumped",
+    hint: "basically any climb at the RRG",
+    img: "./images/pumped.jpg"
+  },
+  {
+    word: "quickdraw",
+    hint: "need this for sport climbing",
+    img: "./images/quickdraw.jpg"
+  },
+  { word: "redpoint", hint: "gotta send", img: "./images/redpoint.jpg" },
+  { word: "runout", hint: "get that trad head", img: "./images/runout.jpg" },
+  { word: "sidepull", hint: "hope it's a jug", img: "./images/sidepull.JPG" },
+  { word: "sloper", hint: "definitely not a jug", img: "./images/sloper.jpg" },
+  { word: "toprope", hint: "the most badass", img: "./images/toprope.jpg" },
+  {
+    word: "traverse",
+    hint: "pitch 15, dawn wall",
+    img: "./images/traverse.jpg"
+  },
+  {
+    word: "whipper",
+    hint: "always take the whip",
+    img: "./images/whipper.jpg"
+  }
 ];
 // grab ids
 let target = document.querySelector("#target");
@@ -45,9 +102,26 @@ let winText = document.querySelector("#wins");
 let lossText = document.querySelector("#losses");
 let guessesLeftText = document.querySelector("#guessesRemaining");
 let hintText = document.querySelector("#hint");
+let winPicDiv = document.querySelector("#winPicDiv");
 
 // watch for events
 window.addEventListener("keyup", getKey);
+
+/*/ Pseudocode
+
+On page load, display title, picture, description etc. + wait for keypress to continue.
+
+On keypress, generate random word (and spans to represent each letter) and begin loop.
+
+For loop - for each keypress, run loop to check if letter (must be lowercase letter) is in target word array and if so, replace ALL matching letter spans with correct letter.
+
+add if no empty spans left (need to figure out how to check this), then run gameWin function
+
+if not, add to letters used array (display this), and decrement guesses left (update on page)
+
+at close of loop run gameOver function
+
+/*/
 
 // select random word and add underscores for each letter
 function word() {
@@ -55,6 +129,7 @@ function word() {
   targetWord = wordList[random].word;
   wordHint = wordList[random].hint;
   hintText.textContent = wordHint;
+  winPicSrc = wordList[random].img;
   // console.log(targetWord);
   targetWord.split("").map(ltr => {
     let span = document.createElement("span");
@@ -127,29 +202,18 @@ function game() {
   }
 }
 
-/*/ Pseudocode
-
-On page load, display title, picture, description etc. + wait for keypress to continue.
-
-On keypress, generate random word (and spans to represent each letter) and begin loop.
-
-For loop - for each keypress, run loop to check if letter (must be lowercase letter) is in target word array and if so, replace ALL matching letter spans with correct letter.
-
-add if no empty spans left (need to figure out how to check this), then run gameWin function
-
-if not, add to letters used array (display this), and decrement guesses left (update on page)
-
-at close of loop run gameOver function
-
-/*/
-
 function checkWin() {
   // check if span arrays still contain any underscores by filtering array
   if (spanArray.filter(item => item.textContent === "_").length === 0) {
     changeText.textContent = `You won!! The word was ${targetWord}`;
     wins++;
     winText.textContent = wins;
-    resetGame();
+    winPic = document.createElement("img");
+    winPic.src = wordList[random].img;
+    winPic.alt = wordList[random].word;
+    winPic.classList.add("img-responsive");
+    winPicDiv.appendChild(winPic);
+    // resetGame();
   }
 }
 
@@ -157,7 +221,7 @@ function resetGame() {
   // reset all scores and delete span nodes
   usedGuesses = [];
   guessText.textContent = "";
-  remaining = 8;
+  remaining = 5;
   guessesLeftText.textContent = remaining;
 
   // found this polyfill - sets first element child, removes it and sets a new first child until it returns false
@@ -173,4 +237,7 @@ function resetGame() {
 
   // it was important to set the targetWord to a falsy value so that word() would recognize it should generate a new one
   targetWord = "";
+
+  // remove the photo
+  winPicDiv.removeChild();
 }
